6aa8e5cb0218a366a87a44c915497501ac0da55f
tanpero <camille-dolma@outlook.com>
2025-04-04 01:42:08 +0800

    Update:
    Add more native stdio functions.

--- src/stdlib/stdio.rs
+++ src/stdlib/stdio.rs
@@ -1,6 +1,7 @@
 use crate::runtime::{Value, Environment};
 use std::rc::Rc;
 use std::cell::RefCell;
+use std::io::{self, Write, BufRead};
 use super::format_value;
 
 pub fn register(env: &Rc<RefCell<Environment>>) {
@@ -10,12 +11,82 @@ pub fn register(env: &Rc<RefCell<Environment>>) {
         vec!["value"],
         |args, _| {
             if let Some(value) = args.get(0) {
-                println!("{}", format_value(value));
+                print!("{}", format_value(value));
                 Ok(Value::Null)
             } else {
-                println!();
+                print!("{}", "".to_string());
                 Ok(Value::Null)
             }
+        },
+    );
+
+    // 添加 println 函数 - 与 print 类似但总是添加换行符
+    super::add_native_fn(
+        env,
+        "println",
+        vec!["value"],
+        |args, _| {
+            if let Some(value) = args.get(0) {
+                println!("{}", format_value(value));
+            } else {
+                println!();
+            }
+            Ok(Value::Null)
+        },
+    );
+
+    // 添加 input 函数 - 从标准输入读取一行文本
+    super::add_native_fn(
+        env,
+        "input",
+        vec!["prompt"],
+        |args, _| {
+            if let Some(prompt) = args.get(0) {
+                print!("{}\n", format_value(prompt));
+                io::stdout().flush().unwrap();
+            }
+            
+            let mut input = String::new();
+            io::stdin().lock().read_line(&mut input).unwrap();
+            
+            // 移除末尾的换行符
+            if input.ends_with('\n') {
+                input.pop();
+                if input.ends_with('\r') {
+                    input.pop();
+                }
+            }
+            
+            Ok(Value::String(input))
+        },
+    );
+
+    // 添加 printf 函数 - 格式化输出，不添加换行符
+    super::add_native_fn(
+        env,
+        "printf",
+        vec!["format", "args"],
+        |args, _| {
+            if let Some(format_str) = args.get(0) {
+                if let Value::String(format_string) = format_str {
+                    let mut result = format_string.clone();
+                    
+                    if args.len() > 1 {
+                        for (i, arg) in args.iter().skip(1).enumerate() {
+                            let placeholder = format!("{{{}}}", i);
+                            result = result.replace(&placeholder, &format_value(arg));
+                        }
+                    }
+                    
+                    print!("{}", result);
+                    io::stdout().flush().unwrap();
+                } else {
+                    print!("{}", format_value(format_str));
+                    io::stdout().flush().unwrap();
+                }
+            }
+            
+            Ok(Value::Null)
         },
     );
 }
